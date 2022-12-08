89ca5d2abe38f90cb3f0a87296f14aebacb04ef3
cksdud0124 <winner456456@naver.com>
2022-12-08 15:23:33 +0900

    .

--- src/test/java/plannet/final_project/entity/MemberTest.java
+++ src/test/java/plannet/final_project/entity/MemberTest.java
@@ -1,116 +1,117 @@
-package plannet.final_project.entity;
-
-import plannet.final_project.dao.*;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import plannet.final_project.dao.MemberRepository;
-
-import javax.persistence.EntityNotFoundException;
-import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-@SpringBootTest
-@Transactional
-//@TestPropertySource(locations = "classpath:application-test.properties")//
-class MemberTest {
-    @Autowired
-    MemberRepository memberRepository;
-    @Autowired
-    PlanRepository planRepository;
-    @Autowired
-    SCALRepository scalRepository;
-    @Autowired
-    SMEMRepository smemRepository;
-    @Autowired
-    SPLANRepository splanRepository;
-    @Autowired
-    QuoteRepository quoteRepository;
-
-    @Test
-    @Rollback(value = false)
-    @DisplayName("멤버 인서트 테스트")
-    public void createData() {
-        //Member
-        for(int i = 1; i < 11; i++) {
-            Member member = new Member();
-            member.setId("test_id_" + i);
-            member.setPwd("test_pwd_" + i);
-            member.setName("이름" + i);
-            member.setNickname("nickname" + i);
-            member.setEmail("test_" + i + "@gmail.com");
-            member.setTel("010-0000-" + i);
-            member.setJoinDate(LocalDateTime.now());
-            String userCode = String.format("%04d", (int)(Math.random() * 9999) + 1);
-            member.setUserCode(userCode);
-            member.setSocial("-");
-            member.setProImg("userdefault.png");
-            memberRepository.save(member);
-        }
-        //plan
-        for(int i = 1; i < 11; i++) {
-            Member member = memberRepository.findById("test_id_" + i).orElseThrow();
-            for(int j = 1; j < 11; j++) {
-                Plan plan = new Plan();
-                plan.setUserId(member);
-                plan.setPlan(i + "의 " + j + "번째 일정");
-                plan.setPlanDate(LocalDate.now());
-                if(j % 2 == 1) plan.setPlanChecked(1);
-                else plan.setPlanChecked(0);
-                planRepository.save(plan);
-            }
-        }
-        //scal
-        for(int i = 1; i < 6; i++){
-            SCAL scal = new SCAL();
-            scal.setCalName(i + "번의 공유 캘린더");
-            scal.setCalMemo(i + "번째 공유 캘린더의 메모입니다");
-            Member member = memberRepository.findById("test_id_" + (i*2)).orElseThrow();
-            scal.setUserId(member);
-            scalRepository.save(scal);
-        }
-        //smem
-        for(int i = 1; i < 6; i++) {
-            SMEM smemOwner = new SMEM();
-            SCAL scal = scalRepository.findById((long) (100 + i)).orElseThrow(EntityNotFoundException::new);
-            smemOwner.setCalNo(scal);
-            smemOwner.setUserId(scal.getUserId());
-            smemOwner.setIsOwner(1);
-            smemRepository.save(smemOwner);
-            for(int j = 1; j < 4; j++) {
-                SMEM smem = new SMEM();
-                smem.setCalNo(scal);
-                Member member = memberRepository.findById("test_id_" + (i + j)).orElseThrow();
-                smem.setUserId(member);
-                smem.setIsOwner(0);
-                smemRepository.save(smem);
-            }
-        }
-        // splan
-        for(int i = 1; i < 6; i++){
-            SPLAN splan = new SPLAN();
-            Member owner = memberRepository.findById("test_id_2").orElseThrow();
-            List<SCAL> scal = scalRepository.findByUserId(owner);
-            splan.setCalNo(scal.get(0));
-            splan.setPlanDate(LocalDate.now());
-            Member member = memberRepository.findById("test_id_" + i).orElseThrow();
-            splan.setUserId(member);
-            if(i % 2 == 0) splan.setPlanChecked(1);
-            else splan.setPlanChecked(0);
-            splan.setPlan("101번 캘린더의" + i + "번째 일정입니다");
-            splanRepository.save(splan);
-        }
-        //quote
-        for(int i = 0; i < 30; i++){
-            Quote quote = new Quote();
-            quote.setQuote("명언" + i);
-            quoteRepository.save(quote);
-        }
-    }
+//package plannet.final_project.entity;
+//
+//import plannet.final_project.dao.*;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import plannet.final_project.dao.MemberRepository;
+//
+//import javax.persistence.EntityNotFoundException;
+//import javax.transaction.Transactional;
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//@SpringBootTest
+//@Transactional
+////@TestPropertySource(locations = "classpath:application-test.properties")//
+//class MemberTest {
+//    @Autowired
+//    MemberRepository memberRepository;
+//    @Autowired
+//    PlanRepository planRepository;
+//    @Autowired
+//    SCALRepository scalRepository;
+//    @Autowired
+//    SMEMRepository smemRepository;
+//    @Autowired
+//    SPLANRepository splanRepository;
+//    @Autowired
+//    QuoteRepository quoteRepository;
+//
+//    @Test
+//    @Rollback(value = false)
+//    @DisplayName("멤버 인서트 테스트")
+//    public void createData() {
+//        //Member
+//        for (int i = 1; i < 11; i++) {
+//            Member member = new Member();
+//            member.setId("test_id_" + i);
+//            member.setPwd("test_pwd_" + i);
+//            member.setName("이름" + i);
+//            member.setNickname("nickname" + i);
+//            member.setEmail("test_" + i + "@gmail.com");
+//            member.setTel("010-0000-" + i);
+//            member.setJoinDate(LocalDateTime.now());
+//            String userCode = String.format("%04d", (int) (Math.random() * 9999) + 1);
+//            member.setUserCode(userCode);
+//            member.setSocial("-");
+//            member.setProImg("userdefault.png");
+//            memberRepository.save(member);
+//        }
+//        //plan
+//        for (int i = 1; i < 11; i++) {
+//            Member member = memberRepository.findById("test_id_" + i).orElseThrow();
+//            for (int j = 1; j < 11; j++) {
+//                Plan plan = new Plan();
+//                plan.setUserId(member);
+//                plan.setPlan(i + "의 " + j + "번째 일정");
+//                plan.setPlanDate(LocalDate.now());
+//                if (j % 2 == 1) plan.setPlanChecked(1);
+//                else plan.setPlanChecked(0);
+//                planRepository.save(plan);
+//            }
+//        }
+//        //scal
+//        for (int i = 1; i < 6; i++) {
+//            SCAL scal = new SCAL();
+//            scal.setCalName(i + "번의 공유 캘린더");
+//            scal.setCalMemo(i + "번째 공유 캘린더의 메모입니다");
+//            Member member = memberRepository.findById("test_id_" + (i * 2)).orElseThrow();
+//            scal.setUserId(member);
+//            scalRepository.save(scal);
+//        }
+//        //smem
+//        for (int i = 1; i < 6; i++) {
+//            SMEM smemOwner = new SMEM();
+//            SCAL scal = scalRepository.findById((long) (100 + i)).orElseThrow(EntityNotFoundException::new);
+//            smemOwner.setCalNo(scal);
+//            smemOwner.setUserId(scal.getUserId());
+//            smemOwner.setIsOwner(1);
+//            smemRepository.save(smemOwner);
+//            for (int j = 1; j < 4; j++) {
+//                SMEM smem = new SMEM();
+//                smem.setCalNo(scal);
+//                Member member = memberRepository.findById("test_id_" + (i + j)).orElseThrow();
+//                smem.setUserId(member);
+//                smem.setIsOwner(0);
+//                smemRepository.save(smem);
+//            }
+//        }
+//        // splan
+//        for (int i = 1; i < 6; i++) {
+//            SPLAN splan = new SPLAN();
+//            Member owner = memberRepository.findById("test_id_2").orElseThrow();
+//            List<SCAL> scal = scalRepository.findByUserId(owner);
+//            splan.setCalNo(scal.get(0));
+//            splan.setPlanDate(LocalDate.now());
+//            Member member = memberRepository.findById("test_id_" + i).orElseThrow();
+//            splan.setUserId(member);
+//            if (i % 2 == 0) splan.setPlanChecked(1);
+//            else splan.setPlanChecked(0);
+//            splan.setPlan("101번 캘린더의" + i + "번째 일정입니다");
+//            splanRepository.save(splan);
+//        }
+//        //quote
+//        for (int i = 0; i < 30; i++) {
+//            Quote quote = new Quote();
+//            quote.setQuote("명언" + i);
+//            quoteRepository.save(quote);
+//        }
+//    }
+//}
 
     // 사용자 정보&개인 일정 달성률/공유캘린더정보 불러오기
 //    public void navList (String userId) {
