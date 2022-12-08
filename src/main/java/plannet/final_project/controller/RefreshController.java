5473a0c90248bf16f652153a27db5da7d42a72d5
cheuvs <cheuvs@daum.net>
2022-12-08 13:57:13 +0900

    .

--- src/main/java/plannet/final_project/controller/RefreshController.java
+++ src/main/java/plannet/final_project/controller/RefreshController.java
@@ -13,7 +13,8 @@ public class RefreshController implements ErrorController {
     private static final String PATH = "/error";
 
     @RequestMapping(value = PATH)
+    // ModelAndView객체는 Model객체와 데이터를 넘길 페이지값을 가진 return을 합친 것이다. 즉, 데이터와 넘길 페이지의 값을 모두 가지고 있는 객체이다.
     public ModelAndView saveLeadQuery() {
-        return new ModelAndView("forward:/");
+        return new ModelAndView("forward:/"); // 참고 사이트 https://mangkyu.tistory.com/51
     }
 }
