40d5f8617d25a0e4b38de4c5f2643f311ce31b85
chan0823 <a3327764@naver.com>
2022-12-09 10:24:07 +0900

    네이버 이메일 인증 구현 완료 프론트 구현중

---
+++ src/main/java/plannet/final_project/controller/EmailController.java
@@ -0,0 +1,25 @@
+package plannet.final_project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import plannet.final_project.dto.EmailAuthRequestDto;
+import plannet.final_project.service.EmailService;
+import plannet.final_project.service.WriteService;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+    @PostMapping("login/mailConfirm")
+    public String mailConfirm(@RequestBody EmailAuthRequestDto emailDto) throws MessagingException, UnsupportedEncodingException, UnsupportedEncodingException {
+
+        String authCode = emailService.sendEmail(emailDto.getEmail());
+        return authCode;
+    }
+}
---
+++ src/main/java/plannet/final_project/dto/EmailAuthRequestDto.java
@@ -0,0 +1,17 @@
+package plannet.final_project.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+
+@Setter
+@Getter
+@ToString
+public class EmailAuthRequestDto {
+
+    @NotEmpty(message = "이메일을 입력해주세요")
+    public String email;
+}
