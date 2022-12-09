bc69deba2523b73c4c7c127a9806805454bd5527
chan0823 <a3327764@naver.com>
2022-12-09 10:24:49 +0900

    네이버 이메일 인증 구현 완료 프론트 구현중

--- src/test/java/plannet/final_project/PlannetApplicationTests.java
+++ src/test/java/plannet/final_project/PlannetApplicationTests.java
@@ -1,13 +1,13 @@
-//package plannet.final_project;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class PlannetApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package plannet.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
