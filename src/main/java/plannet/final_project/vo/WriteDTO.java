5e0d76d2b195d1016c22dbc7568a0a7eb82a791b
lyb0623 <nnoonxnoonn@gmail.com>
2022-12-22 11:25:01 +0900

    DTO 수정

--- src/main/java/plannet/final_project/vo/WriteDTO.java
+++ src/main/java/plannet/final_project/vo/WriteDTO.java
@@ -7,13 +7,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
-
 @Getter @Setter
 public class WriteDTO {
-    private String userId;
-    private LocalDateTime date;
     private List<Map<String, Object>> planList;
-    private Map<String, Object> plan;
     private String diary;
     private boolean isOk;
 }
