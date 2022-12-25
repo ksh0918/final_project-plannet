38b3418cd74fe62fd9feb04efad3fdd44155c9e2
Lee ye bin <nnoonxnoonn@gmail.com>
2022-12-25 18:53:10 +0900

    .

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -11,7 +11,7 @@ import Setting from './Setting/Setting';
 import Home from './Home/Home';
 import Write from './Write/Write';
 import Board from './Board/Board';
-import BoardWrite from './Board/BoardWrite';
+import BoardWrite from './Board/boardWrite';
 import Edit from './Board/Edit';
 import PostView from './Board/PostView';
 import PublicRoute from './Lib/PublicRoute';
